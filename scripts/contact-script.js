fe0facee8e98c21bcc5033f826c863999a6d3553
BlurpyxD <60700831+Blurpyx@users.noreply.github.com>
2021-03-31 00:46:25 +0200

    aangemaakt

---
+++ scripts/contact-script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', (event) => {
+    var adresP = document.getElementById('adres');
+    var contactP = document.getElementById('contact');
+
+    adresP.style.textAlign = 'center';
+    contactP.style.textAlign = 'center';
+
+    db.collection("info").get().then((snapshot) => {
+        var Adres;
+        var Contact;
+        adresP.innerHTML = 'Adres: ';
+        // contactP.innerHTML = '';
+        snapshot.forEach((doc) => {
+            Adres = doc.data().adres;
+            Contact = doc.data().contact;
+            console.log(`${doc.id} => ${Adres.straatnaam}`);
+
+            adresP.innerHTML = '<span style="font-size: 3rem;">' + doc.data().naam + '</span><br>Adres: ';
+            adresP.innerHTML += Adres.straatnaam + ' ' + Adres.nr + ', ' + Adres.stad;
+            contactP.innerHTML += Contact.email + '<br>'
+                 + Contact.telOpt1 + ' / ' + Contact.telOpt2 + '<br>'
+                //  + ;
+        });
+    });
+});
