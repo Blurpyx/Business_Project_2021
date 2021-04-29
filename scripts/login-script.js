d546e043e8e4dea5e22bf5f31b05dbd776898294
BlurpyxD <60700831+Blurpyx@users.noreply.github.com>
2021-04-29 15:43:19 +0200

    JE KAN EINDELIJK INLOGGEN

--- scripts/login-script.js
+++ scripts/login-script.js
@@ -4,13 +4,13 @@ window.addEventListener('load', (e) => {
     loginForm = document.getElementById('login-form');
 
     loginForm.addEventListener('submit', (e) => {
-        // e.preventDefault();
+        e.preventDefault();
 
-        // const Email = loginForm.email.value;
-        // const Pwd = loginForm.pwd.value;
+        const Email = loginForm.email.value;
+        const Pwd = loginForm.pwd.value;
 
-        // auth.signInWithEmailAndPassword(Email, Pwd).then((cred) => {
-        //     window.location.href = 'https://my-agenda-app-uz-gent.web.app/pages/menu.html';
-        // })
+        auth.signInWithEmailAndPassword(Email, Pwd).then((cred) => {
+            window.location.href = '../pages/menu.html';
+        })
     });
 });
