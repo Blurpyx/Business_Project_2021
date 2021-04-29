f3824f83dee124c42ec2e40d90b3ad394ba40d94
Niels <nielsvergote19@gmail.com>
2021-04-29 15:58:13 +0200

    ability to add profile pic, not yet upload to firebase

--- scripts/registreer-script.js
+++ scripts/registreer-script.js
@@ -1,6 +1,34 @@
 window.addEventListener('load', (event) => {
     event.preventDefault();
     regForm = document.getElementById('regForm');
+    
+    $("#profile-photo").click(function(){
+        $("#myfile").click();
+      });
+       const output = document.getElementById("profile-photo");
+       if (window.FileList && window.File && window.FileReader) {
+        document.getElementById("myfile").addEventListener("change", event => {
+            output.src = '';
+            status.textContent = '';
+            file = event.target.files[0];
+            if (!file.type) {
+                status.textContent = 'Error: The File.type property does not appear to be supported on this browser.';
+                return;
+            }
+            if (!file.type.match('image.*')) {
+                status.textContent = 'Error: The selected file does not appear to be an image.'
+                return;
+            }
+            const reader = new FileReader();
+            reader.addEventListener('load', event => {
+                output.src = event.target.result;
+                console.log("Current selected image:" + document.getElementById("myfile").value);
+                console.log("Current slecected image via file.name: " + file.name)
+            });
+            reader.readAsDataURL(file);
+        })
+      }
+
 
     regForm.addEventListener('submit', (event) => {
         event.preventDefault();
